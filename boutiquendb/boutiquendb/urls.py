112b4c8e7d52ac25ecf9414eaf1ab84430115268
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-08-03 12:29:21 -1100

    Update urls

--- boutiquendb/boutiquendb/urls.py
+++ boutiquendb/boutiquendb/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("welcome to my father")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home)
 ]
