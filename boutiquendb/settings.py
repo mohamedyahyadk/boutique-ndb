ef3dab4149b01a2992f95293f2d67f017c2c4faa
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-08-17 00:46:11 -1100

    update settings

--- boutiquendb/settings.py
+++ boutiquendb/settings.py
@@ -72,39 +72,39 @@ WSGI_APPLICATION = 'boutiquendb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-USE_SQLITE = config("USE_SQLITE", default=True, cast=bool)
-if USE_SQLITE:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config("DB_NAME"),
-            'USER': config("DB_USER"),
-            'PASSWORD': config("DB_PASSWORD"),
-            'HOST': config("DB_HOST"),
-            'PORT': config("DB_PORT"),
-        }
-    }
-
-
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-        
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config("DB_NAME"),
-#         'USER': config("DB_USER"),
-#         'PASSWORD': config("DB_PASSWORD"),
-#         'HOST': config("DB_HOST"),
-#         'PORT': config("DB_PORT"),
+# USE_SQLITE = config("USE_SQLITE", default=True, cast=bool)
+# if USE_SQLITE:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / "db.sqlite3",
+#         }
 #     }
-# }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': config("DB_NAME"),
+#             'USER': config("DB_USER"),
+#             'PASSWORD': config("DB_PASSWORD"),
+#             'HOST': config("DB_HOST"),
+#             'PORT': config("DB_PORT"),
+#         }
+#     }
+
+
+DATABASES = {
+    'default': {
+       
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
+    }
+}
 
 
 # Password validation
