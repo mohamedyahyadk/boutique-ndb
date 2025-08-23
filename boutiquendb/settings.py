4a5f489e7158d20d87f1756db77801b0c7fdc589
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-08-23 00:10:33 -1100

    update settings

--- boutiquendb/settings.py
+++ boutiquendb/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'boutiquendb.urls'
 
@@ -147,7 +149,7 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 # STATICFILES_DIRS = [BASE_DIR / "static"]   # global static folder
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR/ 'media'
 
 
 # Default primary key field type
